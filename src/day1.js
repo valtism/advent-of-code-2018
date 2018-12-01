ee53c4e36cdb1f0b0a40181b7a39deab29d8d547
Dan Wood <daniel4wood@icloud.com>
2018-12-01 17:46:57 +1100

    Add timings for functions and increse performance
    Use a Set instead of an array to take part 2 down from 11s to 28ms!

--- src/day1.js
+++ src/day1.js
@@ -2,8 +2,12 @@ const aocLoader = require("aoc-loader");
 require("dotenv").config();
 
 aocLoader(2018, 1).then(data => {
-    console.log(day1part1(data));
-    console.log(day1part2(data));
+    console.time("Part 1");
+    console.log("Part 1: " + day1part1(data));
+    console.timeEnd("Part 1");
+    console.time("Part 2");
+    console.log("Part 2: " + day1part2(data));
+    console.timeEnd("Part 2");
 });
 
 function day1part1(data) {
@@ -13,21 +17,21 @@ function day1part1(data) {
 
 function day1part2(data) {
     const nums = data.split("\n").map(Number);
-    const frequencies = [0];
+    const frequencies = new Set([0]);
     var sum = 0;
-    while (1) {
+    while (true) {
         for (let i = 0; i < nums.length; i++) {
             const num = nums[i];
             sum += num;
-            if (frequencies.includes(sum)) {
+            if (frequencies.has(sum)) {
                 return sum;
             }
-            frequencies.push(sum);
+            frequencies.add(sum);
         }
     }
 }
 
 module.exports = {
     day1part1: day1part1,
-    day1part2: day1part2,
-}
+    day1part2: day1part2
+};
