fcc6d616cc080f371984d31348a0686953cb7a6e
Dan Wood <daniel4wood@icloud.com>
2018-12-01 16:47:31 +1100

    Finished day 1 part 2

--- src/day1.js
+++ src/day1.js
@@ -2,12 +2,32 @@ const aocLoader = require("aoc-loader");
 require("dotenv").config();
 
 aocLoader(2018, 1).then(data => {
-    day1(data);
+    console.log(day1part1(data));
+    console.log(day1part2(data));
 });
 
-function day1(data) {
+function day1part1(data) {
     const nums = data.split("\n").map(Number);
     return nums.reduce((acc, curr) => acc + curr);
 }
 
-module.exports = day1;
+function day1part2(data) {
+    const nums = data.split("\n").map(Number);
+    const frequencies = [0];
+    var sum = 0;
+    while (1) {
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            sum += num;
+            if (frequencies.includes(sum)) {
+                return sum;
+            }
+            frequencies.push(sum);
+        }
+    }
+}
+
+module.exports = {
+    day1part1: day1part1,
+    day1part2: day1part2,
+}
