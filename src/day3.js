c407cdc3571374e3179541b3133d28ccee5f8d02
Dan Wood <daniel4wood@icloud.com>
2018-12-04 12:16:41 +1100

    Finished day 3 part 1

--- src/day3.js
+++ src/day3.js
@@ -1,5 +1,43 @@
 function part1(data) {
-    return 1;
+    const claims = data.split("\n").map(parseClaim);
+
+    const fabric = new Map();
+    claims.forEach(claim => {
+        populateFabric(fabric, claim);
+    });
+
+    return getNumberOverlapped(fabric);
+}
+
+function populateFabric(fabric, claim) {
+    for (let x = claim.xStart; x < claim.xEnd; x++) {
+        for (let y = claim.yStart; y < claim.yEnd; y++) {
+            let coords = x + "," + y;
+            if (fabric.has(coords)) {
+                let count = fabric.get(coords);
+                fabric.set(coords, ++count);
+            } else {
+                fabric.set(coords, 1);
+            }
+        }
+    }
+}
+
+function getNumberOverlapped(fabric) {
+    const gridCounts = Array.from(fabric.values());
+    const overlaps = gridCounts.filter(count => count > 1);
+    return overlaps.length;
+}
+
+function parseClaim(claim) {
+    props = claim.match(/\d{1,}/g).map(Number);
+    return {
+        Id: props[0],
+        xStart: props[1],
+        xEnd: props[1] + props[3],
+        yStart: props[2],
+        yEnd: props[2] + props[4]
+    };
 }
 
 function part2(data) {
--- src/day3.test.js
+++ src/day3.test.js
@@ -1,7 +1,7 @@
 const day3 = require("./day3");
 
 test("Part 1", () => {
-    expect(day3.part1()).toBe(1);
+    expect(day3.part1("#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2")).toBe(4);
 });
 
 test("Part 2", () => {
