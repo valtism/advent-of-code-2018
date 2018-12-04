50f60635d0e0d651db5e9372b05a6938e65833a9
Dan Wood <daniel4wood@icloud.com>
2018-12-04 18:52:02 +1100

    Finished Day 4  😅

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 const day1 = require("./src/day1");
 const day2 = require("./src/day2");
 const day3 = require("./src/day3");
+const day4 = require("./src/day4");
 
-aocLoader(2018, 3).then(data => {
+aocLoader(2018, 4).then(data => {
     console.time("Part 1");
-    console.log("Part 1: " + day3.part1(data));
+    console.log("Part 1: " + day4.part1(data));
     console.timeEnd("Part 1");
     console.time("Part 2");
-    console.log("Part 2: " + day3.part2(data));
+    console.log("Part 2: " + day4.part2(data));
     console.timeEnd("Part 2");
 });
---
+++ src/day4.js
@@ -0,0 +1,124 @@
+function part1(data) {
+    const guardLog = generateGuardLogs(data);
+
+    const totalSleepTimes = getTotalSleepTimes(guardLog);
+    const longestSleeper = getEntryWithLargestValue(totalSleepTimes);
+
+    const sleepyGuardsLog = guardLog.get(longestSleeper[0]);
+    const longestMinute = getEntryWithLargestValue(sleepyGuardsLog);
+
+    return Number(longestSleeper[0]) * longestMinute[0];
+}
+
+function generateGuardLogs(data) {
+    const observations = data.split("\n").map(parseInput);
+    observations.sort(sortByTimestamp);
+    const guardLog = initGuardLog(observations);
+    populateMinutes(observations, guardLog);
+    return guardLog;
+}
+
+function parseInput(observation) {
+    const timestampRe = /(?<=\[).*?(?=\])/;
+    const beginsRe = /begins/;
+    const sleepRe = /sleep/;
+    const idRe = /(?<=#)\d+/;
+    const isBeginShift = beginsRe.test(observation);
+    return {
+        timestamp: new Date(timestampRe.exec(observation)[0]),
+        isBeginShift: isBeginShift,
+        isSleep: sleepRe.test(observation),
+        guardId: isBeginShift ? idRe.exec(observation)[0] : null
+    };
+}
+
+function sortByTimestamp(a, b) {
+    if (a.timestamp < b.timestamp) return -1;
+    if (a.timestamp > b.timestamp) return 1;
+    return 0;
+}
+
+function initGuardLog(observations) {
+    const guardIds = observations
+        .filter(o => o.guardId)
+        .map(o => [o.guardId, new Map()]);
+    const guardLog = new Map(guardIds);
+    return guardLog;
+}
+
+function populateMinutes(observations, guardLog) {
+    let guard = null;
+    let sleepTime = null;
+    observations.forEach(observation => {
+        if (observation.isBeginShift) {
+            guard = guardLog.get(observation.guardId);
+            return;
+        }
+        if (observation.isSleep) {
+            sleepTime = observation.timestamp;
+            return;
+        }
+        if (!observation.isSleep) {
+            const sleepMin = (observation.timestamp - sleepTime) / 1000 / 60;
+            for (
+                let i = sleepTime.getMinutes();
+                i < sleepTime.getMinutes() + sleepMin;
+                i++
+            ) {
+                let minute = i % 60;
+                if (!guard.has(minute)) {
+                    guard.set(minute, 1);
+                } else {
+                    let count = guard.get(minute);
+                    guard.set(minute, ++count);
+                }
+            }
+        }
+    });
+}
+
+function getTotalSleepTimes(guardLog) {
+    const totalSleepTimes = new Map();
+    for (const [id, minutes] of guardLog) {
+        const vals = Array.from(minutes.values());
+        const min = vals.reduce((acc, curr) => acc + curr, 0);
+        totalSleepTimes.set(id, min);
+    }
+    return totalSleepTimes;
+}
+
+function getEntryWithLargestValue(map) {
+    const entries = Array.from(map.entries());
+    return entries.reduce(
+        (longest, curr) => (longest[1] > curr[1] ? longest : curr),
+        [0, 0]
+    );
+}
+
+function part2(data) {
+    const guardLog = generateGuardLogs(data);
+    const mostSleptMinuteLog = getMostSleptMinuteLog(guardLog);
+    const mostSleptMinute = getMostSleptMinute(mostSleptMinuteLog);
+    return Number(mostSleptMinute[0]) * mostSleptMinute[1][0];
+}
+
+function getMostSleptMinuteLog(guardLog) {
+    let mostSleptMinuteLog = new Map();
+    for (const [id, log] of guardLog) {
+        mostSleptMinuteLog.set(id, getEntryWithLargestValue(log));
+    }
+    return mostSleptMinuteLog;
+}
+
+function getMostSleptMinute(mostSleptMinuteLog) {
+    const mostSleptMinuteArr = Array.from(mostSleptMinuteLog.entries());
+    const mostSleptMinute = mostSleptMinuteArr.reduce((acc, curr) =>
+        acc[1][1] > curr[1][1] ? acc : curr
+    );
+    return mostSleptMinute;
+}
+
+module.exports = {
+    part1: part1,
+    part2: part2
+};
