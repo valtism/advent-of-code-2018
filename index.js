004deddf67388dda648bed6d23759a5d2245579c
Dan Wood <daniel4wood@icloud.com>
2018-12-05 17:23:41 +1100

    Finished Day 5

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const day1 = require("./src/day1");
 const day2 = require("./src/day2");
 const day3 = require("./src/day3");
 const day4 = require("./src/day4");
+const day5 = require("./src/day5");
 
-aocLoader(2018, 4).then(data => {
+
+aocLoader(2018, 5).then(data => {
     console.time("Part 1");
-    console.log("Part 1: " + day4.part1(data));
+    console.log("Part 1: " + day5.part1(data));
     console.timeEnd("Part 1");
     console.time("Part 2");
-    console.log("Part 2: " + day4.part2(data));
+    console.log("Part 2: " + day5.part2(data));
     console.timeEnd("Part 2");
 });
---
+++ src/day5.test.js
@@ -0,0 +1,9 @@
+const day5 = require("./day5");
+
+test("Part 1", () => {
+    expect(day5.part1("dabAcCaCBAcCcaDA")).toBe(10);
+});
+
+test("Part 2", () => {
+    expect(day5.part2("dabAcCaCBAcCcaDA")).toBe(4);
+});
