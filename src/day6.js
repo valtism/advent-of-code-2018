324df76b358ce89bc89b8eed8bf3cd0d6d3c2898
Dan Wood <daniel4wood@icloud.com>
2018-12-06 23:46:07 +1100

    Day 6 part 1!

--- src/day6.js
+++ src/day6.js
@@ -2,7 +2,20 @@ function part1(data) {
     const coordStrings = data.split("\n");
     const coords = coordStrings.map(e => e.split(", ").map(Number));
 
-    const lowerBound = Array.from(
+    const lowerBound = getLowerBound(coords);
+    const trimmedCoords = trimCoords(coords, lowerBound);
+    const upperBound = getUpperBound(coords);
+
+    const grid = getDistancesGrid(trimmedCoords, upperBound);
+    const itemsOnPerimeter = getItemsOnPerimeter(grid, upperBound);
+    const itemCounts = getItemCounts(trimmedCoords, grid);
+    let largest = getLargestBoundedArea(itemCounts, itemsOnPerimeter);
+
+    return largest;
+}
+
+function getLowerBound(coords) {
+    return Array.from(
         coords.reduce((acc, curr) => {
             if (curr[0] < acc[0]) {
                 acc[0] = curr[0];
@@ -13,13 +26,17 @@ function part1(data) {
             return acc;
         })
     );
+}
 
-    const trimmedCoords = coords.map(coord => [
+function trimCoords(coords, lowerBound) {
+    return coords.map(coord => [
         coord[0] - lowerBound[0],
         coord[1] - lowerBound[1]
     ]);
+}
 
-    const upperBound = Array.from(
+function getUpperBound(coords) {
+    return Array.from(
         coords.reduce((acc, curr) => {
             if (curr[0] > acc[0]) {
                 acc[0] = curr[0];
@@ -30,17 +47,76 @@ function part1(data) {
             return acc;
         })
     );
+}
 
-    const map = new Map();
-
-    for (i = 0; map.size < upperBound[0] * upperBound[1]; i++) {
-        coords.forEach(coord => {
-            // populate around coord
-            const surroundingCoords = []
-            
-        });
+function getDistancesGrid(trimmedCoords, upperBound) {
+    const grid = new Map();
+    for (let x = 0; x <= upperBound[0]; x++) {
+        for (let y = 0; y <= upperBound[1]; y++) {
+            const distances = trimmedCoords.map((coord, index) => {
+                return {
+                    index: index,
+                    coord: coord,
+                    distance: manhattanDistance([x, y], coord)
+                };
+            });
+            const shortest = distances.reduce((acc, curr) =>
+                acc.distance < curr.distance ? acc : curr
+            );
+            const coordsInRange = distances.filter(
+                distance => distance.distance === shortest.distance
+            ).length;
+            const coordString = "" + x + ", " + y;
+            const closestIndex = coordsInRange === 1 ? shortest.index : ".";
+            grid.set(coordString, closestIndex);
+        }
     }
-    var a = 1;
+    return grid;
+}
+
+function manhattanDistance(p1, p2) {
+    return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+}
+
+function getItemsOnPerimeter(grid, upperBound) {
+    const itemsOnPerimeter = new Set();
+    for (let x = 0; x <= upperBound[0]; x++) {
+        for (let y = 0; y <= upperBound[1]; y++) {
+            const coordString = "" + x + ", " + y;
+            const perimeterIndex = grid.get(coordString);
+            itemsOnPerimeter.add(perimeterIndex);
+            if (x !== 0 && x !== upperBound[0] && y !== upperBound[1]) {
+                // Skip centre
+                y = upperBound[1] - 1;
+            }
+        }
+    }
+    return itemsOnPerimeter;
+}
+
+function getItemCounts(trimmedCoords, grid) {
+    const itemCounts = new Map();
+    for (let i = 0; i < trimmedCoords.length; i++) {
+        itemCounts.set(i, 0);
+    }
+    grid.forEach(value => {
+        let count = itemCounts.get(value);
+        itemCounts.set(value, ++count);
+    });
+    return itemCounts;
+}
+
+function getLargestBoundedArea(itemCounts, itemsOnPerimeter) {
+    let largest = 0;
+    itemCounts.forEach((value, key) => {
+        if (itemsOnPerimeter.has(key) || key === ".") {
+            return;
+        }
+        if (value > largest) {
+            largest = value;
+        }
+    });
+    return largest;
 }
 
 function part2(data) {
