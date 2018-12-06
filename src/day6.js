45f018ca8929bb0d8d6cfbc8e32c608e22af7e14
Dan Wood <dwood@tofi.com.au>
2018-12-06 17:59:21 +1100

    Day 6 WIP

--- src/day6.js
+++ src/day6.js
@@ -1,5 +1,46 @@
 function part1(data) {
-    return 1;
+    const coordStrings = data.split("\n");
+    const coords = coordStrings.map(e => e.split(", ").map(Number));
+
+    const lowerBound = Array.from(
+        coords.reduce((acc, curr) => {
+            if (curr[0] < acc[0]) {
+                acc[0] = curr[0];
+            }
+            if (curr[1] < acc[1]) {
+                acc[1] = curr[1];
+            }
+            return acc;
+        })
+    );
+
+    const trimmedCoords = coords.map(coord => [
+        coord[0] - lowerBound[0],
+        coord[1] - lowerBound[1]
+    ]);
+
+    const upperBound = Array.from(
+        coords.reduce((acc, curr) => {
+            if (curr[0] > acc[0]) {
+                acc[0] = curr[0];
+            }
+            if (curr[1] > acc[1]) {
+                acc[1] = curr[1];
+            }
+            return acc;
+        })
+    );
+
+    const map = new Map();
+
+    for (i = 0; map.size < upperBound[0] * upperBound[1]; i++) {
+        coords.forEach(coord => {
+            // populate around coord
+            const surroundingCoords = []
+            
+        });
+    }
+    var a = 1;
 }
 
 function part2(data) {
--- src/day6.test.js
+++ src/day6.test.js
@@ -1,7 +1,7 @@
 const day6 = require("./day6");
 
 test("Part 1", () => {
-    expect(day6.part1()).toBe(1);
+    expect(day6.part1("1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9")).toBe(17);
 });
 
 test("Part 2", () => {
