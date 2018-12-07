b80c5ee3237d2c71a1c3c301e6bf58383bf1bb0d
Dan Wood <daniel4wood@icloud.com>
2018-12-07 16:54:23 +1100

    Finished Day 7 part 1

--- src/day7.js
+++ src/day7.js
@@ -1,5 +1,46 @@
 function part1(data) {
-    return 1;
+    const instructions = data.split("\n");
+    let dependencies = parseDependencies(instructions);
+    const nodes = getUniqueNodes(dependencies);
+
+    const order = getOrder(nodes, dependencies);
+    return Array.from(order).join("");
+}
+
+function parseDependencies(instructions) {
+    return instructions.map(instruction => {
+        const words = instruction.split(" ");
+        return {
+            dependency: words[1],
+            node: words[7]
+        };
+    });
+}
+
+function getUniqueNodes(dependencies) {
+    const nodes = new Set();
+    dependencies.forEach(dep => {
+        nodes.add(dep.dependency);
+        nodes.add(dep.node);
+    });
+    return nodes;
+}
+
+function getOrder(nodes, dependencies) {
+    const order = new Set();
+    while (nodes.size) {
+        const availableNodes = [];
+        for (const node of nodes) {
+            if (dependencies.every(dep => node !== dep.node)) {
+                availableNodes.push(node);
+            }
+        }
+        availableNodes.sort();
+        order.add(availableNodes[0]);
+        dependencies = dependencies.filter(dep => !order.has(dep.dependency));
+        nodes.delete(...availableNodes);
+    }
+    return order;
 }
 
 function part2(data) {
--- src/day7.test.js
+++ src/day7.test.js
@@ -1,9 +1,19 @@
 const day7 = require("./day7");
 
 test("Part 1", () => {
-    expect(day7.part1()).toBe(1);
+    expect(
+        day7.part1(
+            "Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.F"
+        )
+    ).toBe("CABDFE");
 });
 
 test("Part 2", () => {
-    expect(day7.part2()).toBe(1);
+    expect(
+        day7.part2(
+            "Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.F",
+            2,
+            0
+        )
+    ).toBe(15);
 });
